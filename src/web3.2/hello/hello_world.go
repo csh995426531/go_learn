9c9e5e2cd89fb7e0aa619ba14a1217da3902ae3e
csh995426531 <15670297001@163.com>
2019-04-09 23:44:51 +0800

    web login

--- src/web3.2/hello/hello_world.go
+++ src/web3.2/hello/hello_world.go
@@ -20,8 +20,14 @@ func sayHelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World") //这个写入到w的是输出到客户端
 }
 
+func sayUserName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintf(w, "没办法")
+}
+
 func main() {
-	http.HandleFunc("/", sayHelloWorld)      //设置访问的路由
+	http.HandleFunc("/", sayHelloWorld) //设置访问的路由
+	http.HandleFunc("/name", sayUserName)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
