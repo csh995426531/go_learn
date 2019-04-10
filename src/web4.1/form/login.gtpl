5f5f5d21f68101fc1f2b97fa25e0e22e743bfdf3
csh995426531 <15670297001@163.com>
2019-04-11 00:07:36 +0800

    web form

--- src/web4.1/form/login.gtpl
+++ src/web4.1/form/login.gtpl
@@ -5,7 +5,16 @@
 <body>
 <form action="http://127.0.0.1:9090/login" method="post">
     用户名:<input type="text" name="username">
+    年龄:<input type="text" name="age">
     密码:<input type="password" name="password">
+    <select name="fruit">
+        <option value="apple">apple</option>
+        <option value="pear">pear</option>
+        <option value="banane">banane</option>
+    </select>
+    <input type="checkbox" name="interest" value="football">足球
+    <input type="checkbox" name="interest" value="basketball">篮球
+    <input type="checkbox" name="interest" value="tennis">网球
     <input type="submit" value="登陆">
 </form>
 </body>
--- src/web4.1/form/main.go
+++ src/web4.1/form/main.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -22,7 +24,51 @@ func login(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
+
+		//slice := [] string {"football", "basketball", "tennis"}
+		//a:=Slice_diff(r.Form["interest"],slice)
+		//if a != nil {
+		//	http.Error(w, "interest err", 506)
+		//}
+
+		slice_status := false
+		slice := []string{"apple", "pear", "banane"}
+
+		for _, v := range slice {
+
+			if v == r.Form.Get("fruit") {
+				slice_status = true
+			}
+		}
+
+		if slice_status == false {
+			http.Error(w, "fruit value err", 505)
+		}
+
+		if len(r.Form["username"][0]) == 0 {
+			http.Error(w, "username can't nil", 501)
+			panic("username can't nil")
+		} else if m, _ := regexp.MatchString("^[\\x{4e00}-\\x{9fa5}]+$", r.Form.Get("username")); !m {
+			http.Error(w, "username is zh", 504)
+		}
+
+		age, err := strconv.Atoi(r.Form.Get("age"))
+
+		if err != nil {
+			http.Error(w, "age type err", 502)
+		} else if age > 100 {
+			http.Error(w, "age can't dayu 100", 503)
+		}
+
+		fmt.Println(len(r.Form["username"][0]))
+		fmt.Println(r.Form["username"])
+		fmt.Println(len(r.Form["username"]) == 0)
+
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password", r.Form["password"])
+		fmt.Println("username", r.Form.Get("username"))
+		fmt.Println("username", len(r.Form.Get("username")))
+		fmt.Println("age", r.Form["age"])
+		fmt.Println("fruit", r.Form.Get("fruit"))
 	}
 }
